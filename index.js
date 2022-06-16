e14c739d853087e271d471cf22ddf8758f7f4cec
tmoino <tony.moino@student.moringaschool.com>
2022-06-16 13:57:05 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-function isPalindrome(word) {
+//function isPalindrome(word) {
   // Write your algorithm here
-}
+  function isPalindrome (string){
+    let reversed = string.split ('').reverse('').join('');
+    if (reversed === string) return true;
+    return false;
+  }
+//}
+isPalindrome('abba');
+isPalindrome('racecar');
+isPalindrome ('a');
+isPalindrome('robot');
+isPalindrome('ab');
+
 
 /* 
   Add your pseudocode here
@@ -8,6 +19,11 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  1. abba when reverse will be the same thing
+  2.same to racecars
+  3.a is a single word ata reverse haisaidii
+  4. robot when reversed will be tobor which is not equal to the stirng
+  5.same to this ab is it woll be ba 
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +36,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
 }
 
 module.exports = isPalindrome;
